e4786631f0f2d891b3fc353d91b23e1db5131ba5
GokulRajendran1984 <gookulr@gmail.com>
2025-03-22 18:23:00 -0400

    Create DocSummarizer.py

---
+++ DocSummarizer.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import openai
+from langchain.llms import OpenAI
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+
+st.title("Document Summarizer")
+openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
+if not openai_api_key.startswith('sk-'):
+   st.warning('Please enter your OpenAI API key!', icon='⚠')
+if openai_api_key.startswith('sk-'):
+    uploaded_file = st.file_uploader("Upload a text file", type=["txt"])
+    if uploaded_file is not None:
+       st.write("### Uploaded Document Contents:")
+       file_contents = uploaded_file.read().decode("utf-8")
+       st.write(file_contents)
+       topic=""
+       summarize_button=st.button("Summarize")
+       if summarize_button:
+          llm=ChatOpenAI(api_key=openai_api_key,temperature=0.1,model_name="gpt-3.5-turbo")
+          print(type(file_contents))
+          prompt=ChatPromptTemplate.from_template("Sumarize {topic} in 4 lines")
+          chain=prompt|llm
+          response=chain.invoke({"topic":{file_contents}})
+          print(response)
+          st.write("### Summarized Document:")
+          st.write(response.content)
